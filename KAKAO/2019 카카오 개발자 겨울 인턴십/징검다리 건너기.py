34c7bdbc5913f24ac40872870a776d0e8baaa703
jinan159 <wlsdhks0423@naver.com>
2022-09-22 22:42:00 +0900

    2019 카카오 개발자 겨울 인턴십, 추가 풀이

--- KAKAO/2019 카카오 개발자 겨울 인턴십/징검다리 건너기.py
+++ KAKAO/2019 카카오 개발자 겨울 인턴십/징검다리 건너기.py
@@ -1,26 +1,50 @@
-# 18, 정확성 : 25/25, 효율성 : 0/14  2:52:00
-# 65, 정확성 : 25/25, 효율성 : 0/14  2:42:00 ~ 1:37:00
+# 30, 정확성 : 25/25, 효율성 : 14/14
+# 이 문제는 시간 내 못풀어서, 해설을 보고 다시 풀어봤음
+# 이분탐색을 통해 시간을 줄이는 것이 핵심
 def solution(stones, k):
-    answer = 0
+    left = 1
+    right = max(stones)
 
-    while True:
-        break_cnt = 0
-        for i in range(len(stones)):
-            if stones[i] > 0:
-                stones[i] -= 1
-                break_cnt = 0
+    while left <= right:
+        mid = (left + right) // 2
+        cnt = 0
+
+        for stone in stones:
+            if (stone - mid) <= 0:
+                cnt += 1
+                if cnt >= k:
+                    right = mid - 1
+                    break
             else:
-                break_cnt += 1
-                if break_cnt == k:
-                    return answer
-        answer += 1
+                cnt = 0
+        else:
+            left = mid + 1
 
-    print(answer)
-    return answer
+    print(left)
+
+    return left
+
+
+# # 18, 정확성 : 25/25, 효율성 : 0/14  2:52:00
+# # 65, 정확성 : 25/25, 효율성 : 0/14  2:42:00 ~ 1:37:00
+# def solution(stones, k):
+#     answer = 0
+#
+#     while True:
+#         break_cnt = 0
+#         for i in range(len(stones)):
+#             if stones[i] > 0:
+#                 stones[i] -= 1
+#                 break_cnt = 0
+#             else:
+#                 break_cnt += 1
+#                 if break_cnt == k:
+#                     return answer
+#         answer += 1
+#
+#     print(answer)
+#     return answer
 
 
 solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 3)
-solution([2, 4, 5, 3, 2, 3, 3, 3, 1, 2, 3], 3)  # 3
-solution([2, 3, 2, 5, 5, 5, 5, 5, 1, 2, 1], 3)  # 3
-# [0, 2, 3, 1, 0, 1, 1, 1, 0, 0, 1]
-
+solution([1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1], 2)
--- KAKAO/2019 카카오 개발자 겨울 인턴십/호텔 방 배정.py
+++ KAKAO/2019 카카오 개발자 겨울 인턴십/호텔 방 배정.py
@@ -1,36 +1,103 @@
-# 10, 정확성 : 25/26, 효율성 : 0/7 2:42:00
-def solution(k, room_number):
+# 추가 풀이 2
+# 30, 정확성 : 26/26, 효율성 : 7/7
+# 클래스를 써서 느린거였다;;; 로직에는 문제가 없엇음....
+def solution(k: int, room_number: list):
     answer = []
 
-    room_info_list = [[i + 1, True] for i in range(k)]
+    room_map = dict()
 
-    for num in room_number:
-        for i in range(num, k):
-            room_info = room_info_list[i - 1]
-            if room_info[1]:
-                room_info_list[i - 1][1] = False
-                answer.append(room_info_list[i - 1][0])
-                break
-
-    """
-     1
-        2
-      3
-       4
-         5
-          6
-    7
-    8
-    9
-    10
-    
-    
-    1
-    3
-    4
-    1
-    3
-    1
-    """
+    for room in room_number:
+        next_room = room_map.get(room, 0)
+        if next_room == 0:
+            answer.append(room)
+            room_map[room] = room + 1
+        else:
+            visited = [room]
+            while True:
+                cur_room = next_room
+                next_room = room_map.get(next_room, 0)
+                if next_room == 0:
+                    answer.append(cur_room)
+                    room_map[cur_room] = cur_room + 1
+                    for v in visited:
+                        room_map[v] = cur_room + 1
+                    break
+                visited.append(next_room)
 
     return answer
+
+
+arr = solution(10, [1, 3, 1, 1, 1])
+print(arr)
+
+
+# 추가 풀이 2
+# 10, 정확성 : 26/26, 효율성 : 2/7
+# class Room:
+#     def __init__(self, _no, _next):
+#         self.no = _no
+#         self.isReserved = False
+#         self.next_no = _next
+#
+#
+# def solution(k, room_number):
+#     answer = []
+#
+#     rooms = [Room(i, i+1) for i in range(0, k + 1)]
+#     rooms[-1].next_no = None
+#
+#     for num in room_number:
+#         target_room = rooms[num]
+#
+#         if target_room.isReserved:
+#             visited = []
+#             next_room = rooms[target_room.next_no]
+#             while next_room.isReserved:
+#                 visited.append(next_room.no)
+#                 next_room = rooms[next_room.next_no]
+#             next_room.isReserved = True
+#             answer.append(next_room.no)
+#
+#             for v in visited:
+#                 rooms[v].next_no = next_room.next_no
+#
+#             target_room.next_no = next_room.next_no
+#         else:
+#             target_room.isReserved = True
+#             answer.append(target_room.no)
+#
+#     return answer
+
+
+# 추가 풀이 1
+# 10, 정확성 : 26/26, 효율성 : 0/7
+# def solution(k, room_number):
+#     answer = []
+#
+#     room_reserved = [False for i in range(k + 1)]
+#
+#     for num in room_number:
+#         for i in range(num, len(room_reserved)):
+#             if not room_reserved[i]:
+#                 room_reserved[i] = True
+#                 answer.append(i)
+#                 break
+#
+#     return answer
+
+
+# 10, 정확성 : 25/26, 효율성 : 0/7 2:42:00
+# def solution(k, room_number):
+#     answer = []
+#
+#     room_info_list = [[i + 1, True] for i in range(k)]
+#
+#     for num in room_number:
+#         for i in range(num, k):
+#             room_info = room_info_list[i - 1]
+#             if room_info[1]:
+#                 room_info_list[i - 1][1] = False
+#                 answer.append(room_info_list[i - 1][0])
+#                 break
+#
+#     return answer
